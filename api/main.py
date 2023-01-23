2f581dfb8093587403042ec6f6aa7bfbf3eafcc1
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-01-23 13:09:33 -0300

    rota query add

--- api/main.py
+++ api/main.py
@@ -4,20 +4,28 @@ from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='templates')
 
 
-# @app.route("/query", methods=["GET"])
-# def helloWorld():
-#     args = request.args
-#     hipotenusa = args.get('hipotenusa')
-#     cateto = args.get('cateto')
+@app.route("/query", methods=["GET"])
+def caluladora_raiz():
+    query_params = request.args
+    try:
+        catetoB = float(query_params.get('catetoB'))
+        catetoC = float(query_params.get('catetoC'))
+        hipotenusa = catetoB**2 + catetoC**2
+        
+        return {"hipotenusa":hipotenusa}
+    except TypeError:
+      return {"204":"no_content", "query":"faltam_parametros"}
+    except ValueError:
+        return {"400":"bod_request", "number":"apenas_float_e_int_suportados"}
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    
+
     if request.method == "POST":
-        
+
         catetoB = float(request.form.get('catetoB'))
         catetoC = float(request.form.get('catetoC'))
-        hipotenusa = round((catetoB**2 + catetoC**2), 1)
+        hipotenusa = round(catetoB**2 + catetoC**2)
         
         return render_template('hipotenusa.html', catetoB=catetoB, catetoC=catetoC, hipotenusa=hipotenusa)
         
